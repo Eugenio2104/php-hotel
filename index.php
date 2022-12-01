1c3f7220f5770e335e8588478acad3954c4ed2ae
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-12-01 16:13:45 +0100

    inserimento tabella

--- index.php
+++ index.php
@@ -66,6 +66,48 @@ $hotels = [
 </head>
 
 <body>
+
+  <div class="container">
+    <div class="row">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Parcheggio</th>
+            <th scope="col">Voto</th>
+            <th scope="col">Distanza dal centro/th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+  </div>
+
+
+
   <ul>
     <?php foreach ($hotels as $hotelClass) {
       foreach ($hotelClass as $hotel) {
